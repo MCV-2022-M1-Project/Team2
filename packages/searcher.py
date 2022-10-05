1eeeca72abb3cff6dc7b70df1af4275f85a5ee75
advait2000 <46642008+advait2000@users.noreply.github.com>
2022-10-05 22:18:27 +0200

    All 5 metrics added

--- packages/searcher.py
+++ packages/searcher.py
@@ -26,8 +26,36 @@ class Searcher:
         return results
 
     def chi2_distance(self, histA, histB, eps=1e-10):
-        # Compute the chi squred distance
+        # Compute the chi squared distance
         d = 0.5 * np.sum([((a - b) ** 2) / (a + b + eps) for (a, b) in zip(histA, histB)])
-        
-        # Retrun the chi squared distance
+
+        # Return the chi squared distance
+        return d
+
+    def euclidean_distance(self, histA, histB):
+        # Compute the euclidean distance
+        d = np.sqrt(np.sum([((a - b) ** 2) for (a, b) in zip(histA, histB)]))
+
+        # Return the euclidean distance
+        return d
+
+    def l1_distance(self, histA, histB):
+        # Compute the l1 distance
+        d = np.sum([np.abs(a - b) for (a, b) in zip(histA, histB)])
+
+        # Return the l1 distance
+        return d
+
+    def histogram_intersection(self, histA, histB):
+        # Compute histogram intersection
+        d = np.sum([np.minimum(a, b) for (a, b) in zip(histA, histB)])
+
+        # Return the metric
+        return d
+
+    def hellinger_kernel(self, histA, histB):
+        # Compute the hellinger kernel
+        d = np.sum([np.sqrt(a * b) for (a, b) in zip(histA, histB)])
+
+        # Return the metric
         return d
