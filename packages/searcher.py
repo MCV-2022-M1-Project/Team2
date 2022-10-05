a1a916bfe2ccb7567adbde935a540e5ee563c7db
advait2000 <46642008+advait2000@users.noreply.github.com>
2022-10-05 11:37:04 +0200

    Update searcher.py

--- packages/searcher.py
+++ packages/searcher.py
@@ -4,38 +4,30 @@ import numpy as np
 
 class Searcher:
     def __init__(self, index):
-        # store our index of images
+        # Store index
         self.index = index
 
     def search(self, queryFeatures):
-        # initialize our dictionary of results
+        # Initialize results
         results = {}
 
         # loop over the index
         for (k, features) in self.index.items():
-            # compute the chi-squared distance between the features
-            # in our index and our query features -- using the
-            # chi-squared distance which is normally used in the
-            # computer vision field to compare histograms
+            # Compute chi squared distance
             d = self.chi2_distance(features, queryFeatures)
 
-            # now that we have the distance between the two feature
-            # vectors, we can udpate the results dictionary -- the
-            # key is the current image ID in the index and the
-            # value is the distance we just computed, representing
-            # how 'similar' the image in the index is to our query
+            # Store distances
             results[k] = d
 
-        # sort our results, so that the smaller distances (i.e. the
-        # more relevant images are at the front of the list)
+        # Sort Results
         results = sorted([(v, k) for (k, v) in results.items()])
 
-        # return our results
+        # Return the results
         return results
 
     def chi2_distance(self, histA, histB, eps=1e-10):
-        # compute the chi-squared distance
+        # Compute the chi squred distance
         d = 0.5 * np.sum([((a - b) ** 2) / (a + b + eps) for (a, b) in zip(histA, histB)])
         
-        # return the chi-squared distance
+        # Retrun the chi squared distance
         return d
