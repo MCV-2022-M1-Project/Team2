5968127c99b58acc65fc3784008946188a80d303
advait2000 <46642008+advait2000@users.noreply.github.com>
2022-10-06 23:33:59 +0200

    All types of histogram added

--- packages/histogram.py
+++ packages/histogram.py
@@ -1,5 +1,6 @@
 # Import required packages
 import cv2
+import numpy as np
 
 
 class RGBHistogram:
@@ -16,4 +17,37 @@ class RGBHistogram:
 
         # Flatten the 3D histogram to 1D
         return histogram.flatten()
+
+    def compute_labHistogram(self, image):
+        # Compute a lab Histogram
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+        histogram = cv2.calcHist([image], [0, 1, 2], None, self.bins, [0, 256, 0, 256, 0, 256])
+
+        # Normalize the histogram
+        histogram = cv2.normalize(histogram, histogram)
+
+        # Flatten the 3D histogram to 1D
+        return histogram.flatten()
+
+    def compute_hsvHistogram(self, image):
+        # Compute an HSV histogram
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        histogram = cv2.calcHist([image], [0, 1, 2], None, self.bins, [0, 180, 0, 256, 0, 256])
+
+        # Normalize the histogram
+        histogram = cv2.normalize(histogram, histogram)
+
+        # Flatten the 3D histogram to 1D
+        return histogram.flatten()
+
+    def compute_grayscaleHistogram(self, image):
+        # Compute a grayscale histogram
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        histogram = cv2.calcHist([image], [0], None, [256], [0, 256])
+
+        # Normalize the histogram
+        histogram = cv2.normalize(histogram, histogram)
+
+        # Return the histogram
+        return histogram
 
