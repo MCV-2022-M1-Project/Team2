6084e39bbcbe8043a4e0a4ea4f6d7f670dbec852
AlbertBarreiro <albert.barreiro@estudiantat.upc.edu>
2022-10-14 14:01:18 +0200

    BFS removed

--- threshold.py
+++ threshold.py
@@ -8,43 +8,6 @@ import time
 
 from packages import RemoveText, RemoveBackground
 
-
-
-
-def BFS(thresh):
-    queue = []
-    row, col = thresh.shape
-    mask = np.ones((row,col)).astype("uint8")*255
-    mask[0,0] = 0
-    queue = [(0,0)]
-
-    while len(queue) > 0:
-        i,j = queue[0]
-        queue.pop(0)
-
-
-        if i-1 >= 0 and thresh[i-1, j] <= 0:
-            if (i-1,j) not in queue and mask[i-1,j] > 0: 
-                queue.append((i-1,j))
-                mask[i-1,j] = 0
-
-        if i+1 < row and thresh[i+1, j] <= 0:
-            if (i+1,j) not in queue and mask[i+1,j] > 0:
-                queue.append((i+1,j))
-                mask[i+1,j] = 0
-
-        if j-1 >= 0 and thresh[i, j-1] <= 0:
-            if (i,j-1) not in queue and mask[i,j-1] > 0:
-                queue.append((i,j-1))
-                mask[i,j-1] = 0
-
-        if j+1 < col and thresh[i, j+1] <= 0:
-            if (i,j+1) not in queue and mask[i,j+1] > 0:
-                queue.append((i,j+1))
-                mask[i,j+1] = 0
-        
-    return mask
-
 # Construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", default="./qsd2_w2/00009.jpg", help="Path to the image")
