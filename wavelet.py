15a738a0d85e37419caeb367bb6349e340ef7c67
AlbertBarreiro <albert.barreiro@estudiantat.upc.edu>
2022-10-22 16:44:16 +0200

    wavelet

---
+++ wavelet.py
@@ -0,0 +1,15 @@
+# Import required packages
+import cv2
+from skimage.restoration import (denoise_wavelet, estimate_sigma)
+
+name = '../dataset/qsd2_w3/00010.jpg'
+imagecv = cv2.imread(name)
+
+
+sigma_est = estimate_sigma(imagecv, channel_axis=-1, average_sigmas=True)
+
+im_visushrink2 = denoise_wavelet(imagecv, channel_axis=-1, convert2ycbcr=True,
+                                 method='VisuShrink', mode='soft',
+                                 sigma=sigma_est/4, rescale_sigma=True)
+
+cv2.imwrite("denoised.png", im_visushrink2*255)
