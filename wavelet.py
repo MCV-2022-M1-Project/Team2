3d34b6b454d65803d2eaf161ce1674060736872a
AlbertBarreiro <albert.barreiro@estudiantat.upc.edu>
2022-10-22 17:04:59 +0200

    wavelet with BayesShrink

--- wavelet.py
+++ wavelet.py
@@ -6,10 +6,11 @@ name = '../dataset/qsd2_w3/00010.jpg'
 imagecv = cv2.imread(name)
 
 
-sigma_est = estimate_sigma(imagecv, channel_axis=-1, average_sigmas=True)
+#sigma_est = estimate_sigma(imagecv, channel_axis=-1, average_sigmas=True)
+#im_visushrink2 = denoise_wavelet(imagecv, channel_axis=-1, convert2ycbcr=True,
+#                                 method='VisuShrink', mode='soft',
+#                                 sigma=sigma_est/4, rescale_sigma=True)
+im = denoise_wavelet(imagecv, channel_axis=-1, convert2ycbcr=True,
+                                            method='BayesShrink', mode='hard', rescale_sigma=True)
 
-im_visushrink2 = denoise_wavelet(imagecv, channel_axis=-1, convert2ycbcr=True,
-                                 method='VisuShrink', mode='soft',
-                                 sigma=sigma_est/4, rescale_sigma=True)
-
-cv2.imwrite("denoised.png", im_visushrink2*255)
+cv2.imwrite("denoised.png", im*255)
