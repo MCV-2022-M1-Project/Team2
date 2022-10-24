384e61413a8518240ab761e6c3c9ee17c7c7ec86
advait2000 <46642008+advait2000@users.noreply.github.com>
2022-10-24 22:15:46 +0200

    Extraction file

--- dataset1_extraction.py
+++ dataset1_extraction.py
@@ -3,9 +3,9 @@ import argparse
 import collections
 import pickle
 import cv2
+import numpy as np
 from imutils.paths import list_images
-
-from packages import HistogramDescriptor, RemoveText, Searcher, RGBHistogram, RemoveBackground, TextureDescriptors, read_text
+from packages import HistogramDescriptor, RemoveText, Searcher, TextureDescriptors, read_text, get_text_distance, get_k_images
 from packages.average_precicion import mapk
 
 
@@ -27,8 +27,8 @@ args = vars(ap.parse_args())
 index = {}
 
 # Initialize image descriptor
-# descriptor = HistogramDescriptor((8, 8, 8))
-descriptor = TextureDescriptors()
+descriptor = HistogramDescriptor((8, 8, 8))
+descriptor1 = TextureDescriptors()
 print("Indexing images")
 
 # Use list_images to grab the image paths and loop over them
@@ -39,7 +39,7 @@ for imagePath in list_images(args["index"]):
 
         # Load the image, compute histogram and update the index
         image = cv2.imread(imagePath)
-        features = descriptor.compute_histogram_blocks(image)
+        features = np.concatenate([descriptor.computeHSV(image), descriptor1.compute_hog(image)])
         index[path] = features
 
 # Initialize a Dictionary to store texts
@@ -80,6 +80,7 @@ for imagePath1 in sorted(list_images(args["query1"])):
         text_id = RemoveText(queryImage)
         bbox = text_id.extract_text()
         text = read_text(queryImage, bbox)
+        text_query.append(text)
         image = cv2.rectangle(queryImage1, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 0, 255), 4)
         # cv2.imshow("image", image)
         # cv2.waitKey(0)
@@ -88,13 +89,14 @@ for imagePath1 in sorted(list_images(args["query1"])):
         # queryImage[bbox[1]:bbox[3], bbox[0]:bbox[2]] = mean
 
         # Describe a 3D RGB histogram with 8 bins per channel
-        # desc = HistogramDescriptor((8, 8, 8))
-        desc = TextureDescriptors()
-        queryFeatures = desc.compute_histogram_blocks(queryImage, text_box=bbox)
+        desc = HistogramDescriptor((8, 8, 8))
+        desc1 = TextureDescriptors()
+        queryFeatures = np.concatenate([desc.computeHSV(image), desc1.compute_hog(image)])
 
         # Perform the search
         searcher = Searcher(index)
         results = searcher.search(queryFeatures)
+        # text_results = get_k_images(text=text, index=index_text)
         predicted_query = []
         # print("text", text)
 
@@ -102,8 +104,11 @@ for imagePath1 in sorted(list_images(args["query1"])):
         for j in range(0, 10):
             # Grab the result
             (score, imageName) = results[j]
-            predicted_query.append(int(imageName.replace(".jpg", "")))
+            # (score_text, imageName_text) = text_results[j]
+            # predicted_query.append(int(imageName.replace(".jpg", "")))
             print("\t{}. {} : {:.3f}".format(j + 1, imageName, score))
+            #print("text")
+            # print("\t{}. {} : {:.3f}".format(j + 1, imageName_text, score_text))
 
         # Append the final predicted list
         predicted.append(predicted_query)
