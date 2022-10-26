27e913ef87289d4d320e016db2412c5f97870022
advait2000 <46642008+advait2000@users.noreply.github.com>
2022-10-26 19:11:29 +0200

    ocr

--- packages/__init__.py
+++ packages/__init__.py
@@ -6,5 +6,5 @@ from .histogram import HistogramDescriptor
 from .remove_text import RemoveText
 from .iou import bb_intersection_over_union
 from .texture import TextureDescriptors
-from .ocr import read_text
+from .ocr import read_text, get_text_distance, get_k_images
 from .noise_red import RemoveNoise
