79083dc1307db337cb560ee188acd66a1c34e3ae
AlbertBarreiro <albert.barreiro@estudiantat.upc.edu>
2022-10-29 14:34:24 +0200

    noise_accuracy output changed

--- packages/noise_red.py
+++ packages/noise_red.py
@@ -1,6 +1,7 @@
 import argparse
 import collections
 import pickle
+from tkinter import N
 import cv2
 import numpy as np
 from imutils.paths import list_images
@@ -11,6 +12,8 @@ class RemoveNoise:
 
     qsd1_w3_y = [0,1,6,8,11,13,16,17,20,23,24,26,29] # noisy images from qsd1_w3 dataset
     qsd1_w3_n = [2,3,4,5,7,9,10,12,14,15,18,19,21,22,25,27,28] # not noisy images from qsd1_w3 dataset
+    qsd2_w3_y = [0,3,4,5,10,12,16,18,19] # noisy images from qsd1_w3 dataset
+    qsd2_w3_n = [1,2,6,7,8,9,11,13,14,15,17,20,21,22,23,24,25,26,27,28,29] # not noisy images from qsd1_w3 dataset
 
     def __init__(self,image):
         self.image = image
@@ -61,8 +64,29 @@ class RemoveNoise:
             return im_visushrink2
 
     def noise_accuracy_qsd1_w3(self, name):
-        #getting the name of the image tells if the detection is correct
-        gt_noisy = int(name) in self.qsd1_w3_y
+        return self.noise_accuracy(name, self.qsd1_w3_y)
 
-        return gt_noisy == self.hasSaltNPepper(verbose=True)
+    
+    def noise_accuracy_qsd2_w3(self, name):
+        return self.noise_accuracy(name, self.qsd2_w3_y)
+
+    def noise_accuracy(self, name, gt_y):
+            #getting the name of the image tells if the detection is correct
+            tp = 0
+            fn = 0
+            fp = 0
+            tn = 0
+            n = int(name)
+            if n in gt_y: # Actual condition P
+                if self.hasSaltNPepper(verbose=True):  # Predicted P
+                    tp = 1
+                else: # Predicted N
+                    fn = 1
+            elif not n in gt_y: # Actual condition N
+                if self.hasSaltNPepper(verbose=True):  # Predicted P
+                    fp = 1
+                else: # Predicted N
+                    tn = 1
+
+            return (tp, fn, fp, tn)
 
--- test_noise.py
+++ test_noise.py
@@ -8,17 +8,29 @@ from packages import RemoveNoise
 
 
 
-path = "../dataset/qsd1_w3/"
+path = "../dataset/qsd2_w3/"
 
-count = 0
-total = 0
+
+tp = 0
+fn = 0
+fp = 0
+tn = 0
+
 for imagePath in list_images(path):
     if "jpg" in imagePath and not "non_augmented" in imagePath:
         # Load the image
         print(imagePath)
         image = cv2.imread(imagePath)
         rm_v = RemoveNoise(image)
-        if rm_v.noise_accuracy_qsd1_w3(imagePath[-9:-4]):
-            count += 1
-        total += 1 
-print("Accuracy:", count*100/total)
+        tp_a, fn_a, fp_a, tn_a = rm_v.noise_accuracy_qsd2_w3(imagePath[-9:-4])
+        tp += tp_a
+        fn += fn_a
+        fp += fp_a
+        tn += tn_a
+precission = (tp)/(tp+fp)
+recall = (tp)/(tp+fn)
+f1 = 2*(precission*recall)/(precission+recall)
+
+print("Precission:", precission)
+print("Recall:", recall)
+print("F1:", f1)
