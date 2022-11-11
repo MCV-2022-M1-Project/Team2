5db94e988b72291aec013f3ce95446cb66e580ac
AlbertBarreiro <albert.barreiro@estudiantat.upc.edu>
2022-11-11 23:48:16 +0100

    sad sad

--- average_hist.py
+++ average_hist.py
@@ -3,6 +3,7 @@ import argparse
 import cv2
 from imutils.paths import list_images
 from matplotlib import pyplot as plt
+import numpy as np
 
 # Construct argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -10,43 +11,47 @@ ap.add_argument("-i", "--index", default="../dataset/bbdd", help="Path to the im
 
 
 
-pictures = []
-palets = []
-bins = 3
-div = 256/bins
-imagePaths = []
+
 sv = []
+contours = []
 
-
-for imagePath in sorted(list_images("D:/Documentos/Uni/Master/M1/dataset/BBDD")):
+for imagePath in sorted(list_images("../dataset/BBDD")):
     if "jpg" in imagePath:
         print(imagePath)
-        
-        imagePaths.append(imagePath)
+
         image = cv2.imread(imagePath)
-        hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+        hls = cv2.cvtColor(image,cv2.COLOR_BGR2HLS)
 
-        v = 0
-        s = 0
-        count = 0
-        for i in range(hsv.shape[0]):
-            for j in range(hsv.shape[1]):
-                v += hsv[i][j][2]
-                s += hsv[i][j][1]
-                count += 1
-        sv.append((int)(v / count) + (int)(s / count))
+        r,c = hls[:,:,1].shape
+        l = np.sum(hls[:,:,1])
+        s = np.sum(hls[:,:,2])
 
-svsort = sv
-svsort.sort()
+        count = r*c
+
+        sv.append(1/(l / count)+1/(s / count)) # lower l and s better
+
+        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.medianBlur(image_gray, 7)
+        r,c = blurred.shape
+        canny = cv2.Canny(blurred, 10, 100)
+        thresh = np.sum(canny > 0)
+        r_tresh = (thresh/(r*c)) # bigger thresh better
+
+
+        contours.append(r_tresh)
+
+sv = [float(i)/max(sv) for i in sv]
+contours = [float(i)/max(contours) for i in contours]
+
+
 best = []
+for i in range(len(sv)):
+    mult_sv = 0.5
+    mult_cont = 0.5
+    score = mult_sv * sv[i] + mult_cont * (1-abs(contours[i]-0.6))
+    best.append(((str(i).zfill(5)), score))
 
-for val in svsort[:5]:
-    index = sv.index(val)
-    best.append(imagePaths[index])
-    imagePaths.pop(index)
-    sv.pop(index)
+best.sort(key = lambda x: x[1], reverse=True)
 
-print("Best:")
-for path in best:
-    print("\t" + path)
-    plt.imshow(cv2.imread(path), 'gray'),plt.show()
+
+print(best[:5])
